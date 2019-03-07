fa9696e1aaca330306ceaedee7b84003574b2a66
Ryan <arcticoder@users.noreply.github.com>
2019-03-06 19:02:12 -0800

    Beyond the Basics - Upload files

--- app/Http/Controllers/ContentsController.php
+++ app/Http/Controllers/ContentsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ContentsController extends Controller
 {
@@ -12,5 +13,22 @@ class ContentsController extends Controller
         $data = [];
         $data['version'] = '0.1.2';
         return view('contents/home', $data);
+    }
+
+    public function upload(Request $request)
+    {
+        $data = [];
+        if( $request->isMethod('post') )
+        {
+            $this->validate(
+                $request,
+                [
+                    'image_upload' => 'mimes:jpeg,bmp,png'
+                ]
+            );
+            Input::file('image_upload')->move('images', 'attractions.jpg');
+            return redirect('/');
+        }
+        return view('contents/upload', $data);
     }
 }
